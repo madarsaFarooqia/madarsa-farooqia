af64ccbb45589404946bdffd926602dd5245ea26
Mohammad Sohail <sohail1613@gmail.com>
2026-06-07 06:10:53 +0300

    test

--- src/App.js
+++ src/App.js
@@ -254,7 +254,7 @@ function App() {
     <QueryClientProvider client={queryClientInstance}>
       <AuthProvider>
         <LanguageProvider>
-          <Router basename="/madarsa-farooqia">
+          <Router>
             <AuthenticatedApp />
           </Router>
 
